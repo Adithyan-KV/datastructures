b3d4f3d7a7f3fc671d68f6b489600696dc02c04a
Adithyan-KV <adithyankv007@gmail.com>
2020-09-04 02:11:18 +0530

    Length of linked list implemented
     -The len function and associated tests were written

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -2,3 +2,23 @@ class Node():
 
     def __init__(self, data=None):
         self.data = data
+        self.next = None
+
+
+class LinkedList():
+
+    def __init__(self):
+        self.head = None
+
+    def __len__(self):
+        current_node = self.head
+        length = 0
+        while current_node:
+            length += 1
+            current_node = current_node.next
+        return length
+
+    def insert_beginning(self, node):
+        if not self.head:
+            self.head = node
+            node.next = None
--- linked_list/test_linked_list.py
+++ linked_list/test_linked_list.py
@@ -4,8 +4,16 @@ import linked_list
 
 class TestLinkedList(unittest.TestCase):
 
-    def test_access_empty_list(self):
+    def test_empty_list(self):
         l = linked_list.LinkedList()
+        self.assertEqual(l.head, None)
+        self.assertEqual(len(l), 0)
+
+    def test_length_of_one_item_list(self):
+        l = linked_list.LinkedList()
+        n1 = linked_list.Node(1)
+        l.insert_beginning(n1)
+        self.assertEqual(len(l), 1)
 
 
 if __name__ == "__main__":
