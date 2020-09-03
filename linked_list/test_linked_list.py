9f933b52920aca21856146f9665ce971ab44f8c4
Adithyan-KV <adithyankv007@gmail.com>
2020-09-04 01:43:00 +0530

    Set up boilerplate code for linked lists

---
+++ linked_list/test_linked_list.py
@@ -0,0 +1,12 @@
+import unittest
+import linked_list
+
+
+class TestLinkedList(unittest.TestCase):
+
+    def test_access_empty_list(self):
+        l = linked_list.LinkedList()
+
+
+if __name__ == "__main__":
+    unittest.main()
