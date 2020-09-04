1cc3c0339903415034562f7eaa8bf4b9460f6a54
Adithyan-KV <adithyankv007@gmail.com>
2020-09-05 00:09:11 +0530

    Added __str__ method
     - An str method was added for printing out a formatted linked list
     - Associated tests for str method were written

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -18,10 +18,31 @@ class LinkedList():
             current_node = current_node.next
         return length
 
+    def __str__(self):
+        current_node = self.head
+        data_list = []
+        string = ""
+        while current_node is not None:
+            data_list.append(f'({current_node.data})->')
+            current_node = current_node.next
+        data_list.append('None')
+        return string.join(data_list)
+
     def insert_beginning(self, node):
+        if self.head is None:
+            self.head = node
+        else:
+            node.next = self.head
+            self.head = node
+
+    def insert_end(self, node):
         if self.head is None:
             self.head = node
             node.next = None
         else:
-            node.next = self.head
-            self.head = node
+            current_node = self.head
+            while current_node is not None:
+                if current_node.next is None:
+                    current_node.next = node
+                    break
+                current_node = current_node.next
--- linked_list/test_linked_list.py
+++ linked_list/test_linked_list.py
@@ -31,6 +31,33 @@ class TestLinkedList(unittest.TestCase):
         self.assertEqual(len(l), 2)
         self.assertEqual(l.head, n2)
 
+    def test_insert_to_end_of_empty_list(self):
+        l = linked_list.LinkedList()
+        n1 = linked_list.Node(1)
+        l.insert_end(n1)
+        self.assertEqual(len(l), 1)
+        self.assertEqual(l.head, n1)
+
+    def test_insert_to_end_of_non_empty_list(self):
+        l = linked_list.LinkedList()
+        n1 = linked_list.Node(1)
+        n2 = linked_list.Node(2)
+        l.insert_beginning(n1)
+        l.insert_end(n2)
+        self.assertEqual(len(l), 2)
+
+    def test_str_empty_list(self):
+        l = linked_list.LinkedList()
+        self.assertEqual(str(l), "None")
+
+    def test_str_non_empty_list(self):
+        l = linked_list.LinkedList()
+        n1 = linked_list.Node(1)
+        n2 = linked_list.Node(2)
+        l.insert_beginning(n1)
+        l.insert_end(n2)
+        self.assertEqual(str(l), "(1)->(2)->None")
+
 
 if __name__ == "__main__":
     unittest.main()
